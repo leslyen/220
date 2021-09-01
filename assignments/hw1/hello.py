cfc01f075513968ec31fa2b9cfe2b2bc8fe10062
leslyen <endarald@g.cofc.edu>
2021-09-01 19:00:58 -0400

    finished assignment :)

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: Lesly Endara
+hello.py
+
+Problem: this program shows a hello statement
+
+
+certification of Authenticity:
+I certify that this is entirely my own work
+"""
+
+
+def main():
+    print("hello, world!")
