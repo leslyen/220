ac1f493576504b72c5de83d85a9967aad7920b6d
leslyen <endarald@g.cofc.edu>
2021-11-29 12:47:26 -0500

    Extra credit fix.

--- labs/lab12/lab12.py
+++ labs/lab12/lab12.py
@@ -1,7 +1,6 @@
 """
 Name: Lesly Endara
 lab12.py
-create a hangman game
 certification of Authenticity:
 I certify that this is entirely my own work
 """
@@ -37,11 +36,13 @@ def is_in_linear(search_val, values):
     return answer
 """
 
-def find_and_remove_first(list, value):
-    part = list.index(value)
-    list.remove(value)
-    list.insert(part, "Lesly")
-    print(list)
+
+def find_and_remove_first(lists, value):
+    part = lists.index(value)
+    lists.remove(value)
+    lists.insert(part, "Lesly")
+    print(lists)
+
 
 def good_input():
     number = eval(input("please enter a number from 1-10"))
@@ -53,9 +54,10 @@ def good_input():
     print("yay, you entered", number, "thank you")
     return number
 
-def  num_digits():
+
+def num_digits():
     number = eval(input("please enter a positive number"))
-    while number >= 0:
+    while number > 0:
         times = 0
         while number > 0:
             number = number // 10
@@ -64,15 +66,20 @@ def  num_digits():
         number = eval(input("please enter a positive number"))
 
 
-"""
 def hi_lo_game():
     number = randint(1, 100)
-    guess = eval(input("try to guess my number (it's between 1-100"))
-    while guess > number or guess < number:
+    guess = eval(input("try to guess my number (it's between 1-100)"))
+    times = 1
+    while times < 7:
         if guess > number:
-            guess = eval(input(""))
-            didn't get to it
-"""
+            guess = eval(input("sorry, That's to high, try again"))
+        elif guess < number:
+            guess = eval(input("sorry, That's to low, try again"))
+        times += 1
+    if guess == number:
+        print("You win in", times, "guesses!")
+    else:
+        print("Sorry, you lose. The number was", number)
 
 
 def main():
@@ -84,6 +91,7 @@ def main():
     print(algorithms.is_in_linear(79, file_list))
     good_input()
     num_digits()
+    hi_lo_game()
 
     pass
 
